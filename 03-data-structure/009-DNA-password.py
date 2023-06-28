72e87d54952978f8cf4a2cb9072417975b74f60c
sarifor <iread1q84@gmail.com>
2023-06-28 15:23:59 +0900

    Update explanation for 009

--- 03-data-structure/009-DNA-password.py
+++ 03-data-structure/009-DNA-password.py
@@ -1,16 +1,16 @@
 # Baekjoon 12891
+# 문자열을 입력받아, 각 문자가 개수 조건에 맞는지, 지정 범위를 이동시켜 가며 검증하고, 맞는다면 유효한 비밀번호로 친다.
 
-checkList = [0] * 4 # 각 문자의 개수 조건
-myList = [0] * 4 # 현재 각 문자의 개수
-checkSecret = 0 # (A, C, G, T 중) 몇 개의 문자와 관련된 개수를 충족했는지 판단하는 수
+checkList = [0] * 4 
+myList = [0] * 4
+checkSecret = 0
 
-# 새로 들어오는 문자열 처리
 def myadd(c):
     global checklist, myList, checkSecret
     if c == 'A':
         myList[0] += 1
-        if myList[0] == checkList[0]: # 'A'가 지정한 개수만큼 있으면,
-            checkSecret += 1 # 1개 문자가 개수 충족했다고 판단
+        if myList[0] == checkList[0]:
+            checkSecret += 1
     elif c == 'C':
         myList[1] += 1
         if myList[1] == checkList[1]:
@@ -24,10 +24,9 @@ def myadd(c):
         if myList[3] == checkList[3]:
             checkSecret += 1
             
-# 제거되는 문자열 처리
 def myremove(c):
     global checkList, myList, checkSecret
-    if c == 'A': # A가 들어오면, checkSecret과 myList에서 A와 관련된 개수를 1 삭감
+    if c == 'A':
         if myList[0] == checkList[0]: 
             checkSecret -= 1
         myList[0] -= 1 
@@ -44,26 +43,21 @@ def myremove(c):
             checkSecret -= 1
         myList[3] -= 1
 
-# S, P, A, checkList를 입력받음
 S, P = map(int, input().split())
 Result = 0
 A = list(input())
 checkList = list(map(int, input().split()))
 
-# 값이 0이면 비밀번호 개수가 이미 만족되었다는 뜻이니, checkSecret을 1 늘림
 for i in range(4):
     if checkList[i] == 0:
         checkSecret += 1
 
-# 초기 P 부분 문자열 처리
 for i in range(P):
     myadd(A[i])
 
-# 4 자릿수와 관련된 크기가 모두 충족될 때 유효한 비밀번호 개수
-if checkSecret == 4: # A, C, G, T 각 문자의 개수가 모두 조건을 만족하면(4), 유효한 비밀번호로 판단
+if checkSecret == 4:
     Result += 1
 
-# 한 칸씩 이동하면서 새로 들어오는 문자열과 제거되는 문자열을 처리
 for i in range(P, S):
     j = i - P
     myadd(A[i])
