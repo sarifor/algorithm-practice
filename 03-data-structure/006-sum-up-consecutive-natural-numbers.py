adf3a6b1ece442bce2ee9b6c98d0e3a1a323df82
sarifor <iread1q84@gmail.com>
2023-06-28 11:13:46 +0900

    Update English Explanation for 006

--- 03-data-structure/006-sum-up-consecutive-natural-numbers.py
+++ 03-data-structure/006-sum-up-consecutive-natural-numbers.py
@@ -1,11 +1,5 @@
 # Baekjoon 2018
-# n을 입력받아, n과 같은 연속된 자연수 합(sum)을 나타내는 가짓수를 출력한다.
-# 연속된 수는 시작 인덱스(start_index)와 종료 인덱스(index)를 지정하여 표현한다.
-# 합이 n에 모자라면, 끝에 자연수(end_index)를 하나 더 더하고, 끝에 더할 자연수(end_index)를 1 늘린다.
-# 합이 n을 넘으면, 앞의 자연수(start_index)를 하나 제외하고, 앞에서 제외할 자연수(start_index)를 1 늘린다.
-# 합이 n이 되면 카운트하고(가짓수 집계), 끝에 더할 자연수(end_index)를 1 늘린다.
-# 끝에 더할 자연수(end_index)가 n과 같아지기 직전까지 처리를 반복한다.
-# 요약하면, n과 연속된 자연수의 합이 같아질 때까지 연속된 자연수 배열을 앞뒤로 범위 조절하여, 같아지는 경우를 발견하면, 해당 가짓수를 카운트하는 것이다.
+# Count the number of the cases if the sum of consecutive natural numbers is equal to n when adjusted forward and backward.
 
 n = int(input())
 count = 1
