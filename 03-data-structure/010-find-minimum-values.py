c9e348083a0db6b4ef8c4e9056c541b7076973cc
sarifor <iread1q84@gmail.com>
2023-07-05 15:03:31 +0900

    Add explanation for 010

--- 03-data-structure/010-find-minimum-values.py
+++ 03-data-structure/010-find-minimum-values.py
@@ -6,7 +6,8 @@ mydeque = deque() # 데이터를 담을 덱 자료구조
 now = list(map(int, input().split())) # 주어진 숫자 데이터를 가지는 리스트
 
 for i in range(N): # N만큼 반복
-    # 덱의 마지막 위치에서부터 현재 값보다 큰 값은 덱에서 제거하고, 덱의 마지막 위치에 현재 값을 저장
+    # 덱의 마지막 위치에서부터 현재 값보다 큰 값은 덱에서 제거하고, 덱의 마지막 위치에 현재 값을 저장.
+    # 덱을 이용하여 오름차순 정렬 효과를 봄.
     while mydeque and mydeque[-1][0] > now[i]: # 덱의 마지막 번째의 '값' 
         mydeque.pop()
     mydeque.append((now[i], i)) # (값, 인덱스) 순 저장
