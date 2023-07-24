7332f792dfe89d4da34de4b9ae62a2bc58cb517e
sarifor <iread1q84@gmail.com>
2023-07-25 08:39:23 +0900

    Update explanation for 017

--- 04-sort/017-sort-digits-of-number-in-desending-order.py
+++ 04-sort/017-sort-digits-of-number-in-desending-order.py
@@ -6,10 +6,10 @@ A = list(input())
 
 for i in range(len(A)):
     Max = i
-    for j in range(i+1, len(A)):
+    for j in range(i+1, len(A)): #i번째 요소와 i번째 이후(j) 요소를 비교하여 최댓값을 찾고,
         if A[j] > A[Max]:
             Max = j
-    if A[i] < A[Max]:
+    if A[i] < A[Max]: # 최댓값과 i번째 요소의 위치를 바꾼다.
         temp = A[i]
         A[i] = A[Max]
         A[Max] = temp
