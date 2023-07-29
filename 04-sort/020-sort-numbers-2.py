bd3b0aaa7435ef79d3d73fcb3b9c979c9788af83
sarifor <iread1q84@gmail.com>
2023-07-29 15:27:37 +0900

    Update english explanation for 020

--- 04-sort/020-sort-numbers-2.py
+++ 04-sort/020-sort-numbers-2.py
@@ -1,7 +1,8 @@
 # Backjoon 2751
-# "입력받은 그룹(A)의 복사본(temp)을 만들어, 
-# 해당 복사본을 절반으로 쪼개어, 각각의 인덱스를 비교하고, 
-# 더 작은 값을 A에 덮어씌움으로, 오름차순 정렬한다."
+# Divide the array A into two, 
+# select smaller value comparing two temp arrays,
+# save it into array A,
+# and check if any element was left.
 
 import sys
 input = sys.stdin.readline
@@ -18,12 +19,12 @@ def merge_sort(s, e):
         tmp[i] = A[i]
 
     k = s
-    index1 = s # 첫 번째 그룹의 인덱스
-    index2 = m + 1 # 두 번째 그룹의 인덱스
+    index1 = s
+    index2 = m + 1
 
-    while index1 <= m and index2 <= e: # 첫 그룹의 m번째까지와 두 번째 그룹의 e번째까지 동안
-        if tmp[index1] > tmp[index2]: # 첫 번째 그룹의 인덱스가 두 번째 그룹의 인덱스보다 크다면
-            A[k] = tmp[index2] # (더 작은 값인) 두 번째 그룹의 값을 A 리스트에 넣고
+    while index1 <= m and index2 <= e:
+        if tmp[index1] > tmp[index2]:
+            A[k] = tmp[index2]
             k += 1
             index2 += 1
         else:
@@ -31,12 +32,12 @@ def merge_sort(s, e):
             k += 1
             index1 += 1
 
-    while index1 <= m: # 한쪽 그룹이 모두 선택된 후, 남아 있는 값 정리 (다음 index1)
+    while index1 <= m:
         A[k] = tmp[index1]
         k += 1
         index1 += 1
     
-    while index2 <= e: # 한쪽 그룹이 모두 선택된 후, 남아 있는 값 정리 (2) (다음 index2)
+    while index2 <= e:
         A[k] = tmp[index2]
         k += 1
         index2 += 1
