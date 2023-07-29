1f9f075152a489b0c5d05c2ea52ddc84d47634a1
sarifor <iread1q84@gmail.com>
2023-07-29 16:58:13 +0900

    Update explanation for 022

--- 04-sort/022-sort-numbers-3.py
+++ 04-sort/022-sort-numbers-3.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 N = int(input())
 count = [0] * 10001
 
-for i in range(N):
+for i in range(N): # N만큼 반복하며, count 리스트에 현재 수에 해당하는 index의 값 1 증가
     count[int(input())] += 1
 
 for i in range(10001):
