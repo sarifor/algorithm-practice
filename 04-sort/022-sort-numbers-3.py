858e866b8d4bddffb91157e04f0066a91eb4a8e1
sarifor <iread1q84@gmail.com>
2023-07-29 17:20:59 +0900

    Update english explanation for 022

--- 04-sort/022-sort-numbers-3.py
+++ 04-sort/022-sort-numbers-3.py
@@ -1,14 +1,18 @@
 # Backjoon 10989
+# Take a count array to store the count of each number.
+# Store the count of each number in the count array.
+# If any number repeats itself, simply increase its count.
+# Print each index of count array as much as its element.
 
 import sys
 input = sys.stdin.readline
 N = int(input())
 count = [0] * 10001
 
-for i in range(N): # N만큼 반복하며, count 리스트에 현재 수에 해당하는 index의 값 1 증가
+for i in range(N):
     count[int(input())] += 1
 
 for i in range(10001):
-    if count[i] != 0: # count[i]의 값이 0이 아니면, count[i]의 값만큼 i를 반복해 출력
+    if count[i] != 0:
         for _ in range(count[i]):
             print(i)
