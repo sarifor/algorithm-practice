30b1d0438e52dd12fbd009e4466b1b1873307d03
sarifor <iread1q84@gmail.com>
2023-08-04 17:18:27 +0900

    Update English explanation for 023

--- 05-search/023-calculate-connected-components.py
+++ 05-search/023-calculate-connected-components.py
@@ -1,30 +1,30 @@
 # Baekjoon 11724
-# 그래프를 준비하고, 모든 노드를 방문할 때까지 DFS를 진행한다.
-# DFS 실행 횟수는 연결 요소 개수와 같다.
+# Call DFS recursively until all nodes are visited.
+# The number of DFS runs is equal to the number of connected components.
 
 import sys
 sys.setrecursionlimit(10000)
 input = sys.stdin.readline
-n, m = map(int, input().split()) # 노드 개수, 에지 개수
+n, m = map(int, input().split())
 A = [[] for _ in range(n+1)]
 visited = [False] * (n+1)
 
 def DFS(v):
-   visited[v] = True # 1에 2, 5가 연결되어 있다면, 현재 노드(1)를 방문 기록 하고
-   for i in A[v]: # 현재 노드의 연결 노드(2, 5) 중
-      if not visited[i]: # 방문하지 않은 노드로(예: visited[2] == False)
-         DFS(i) # DFS 실행
+   visited[v] = True
+   for i in A[v]:
+      if not visited[i]:
+         DFS(i)
 
 for _ in range(m):
-    s, e = map(int, input().split()) # 인접 리스트에 그래프 데이터 저장
+    s, e = map(int, input().split())
     A[s].append(e)
     A[e].append(s)
 
 count = 0
 
-for i in range(1, n+1): # n의 개수만큼 반복하며
-    if not visited[i]: # 방문하지 않은 노드가 있다면
-      count += 1 # 연결 개수 값 1 증가하고
-      DFS(i) # DFS 실행
+for i in range(1, n+1):
+    if not visited[i]:
+      count += 1
+      DFS(i)
 
 print(count)
