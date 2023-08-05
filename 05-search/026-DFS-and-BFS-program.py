2b97a4748b53c4a45ad52d72297724bb2adf4f46
sarifor <iread1q84@gmail.com>
2023-08-05 12:12:09 +0900

    Update English Explanation for 026

--- 05-search/026-DFS-and-BFS-program.py
+++ 05-search/026-DFS-and-BFS-program.py
@@ -1,20 +1,18 @@
-# Backjoon 1260 (원래 문제의 DFS 처리 삭제)
-# DFS는 최대 깊이까지 탐색을 하는 반면, BFS는 인접 노드를 우선으로 탐색을 한다.
-# 예를 들어 '1 -> 2, 3'이고, '2 -> 5, 6'이라면,
-# "DFS는 1, 2, 5, 6 ... 순으로 가는데,
-# BFS는 1, 2, 3(인접 노드), 5, 6 ... 순으로 간다."
+# Backjoon 1260 (DFS omitted)
+# DFS proceeds through the nodes as far as possible until reaching the node with no unvisited nearby node,
+# while BFS walks through all nodes on the same level before moving on to the next level.
 
 from collections import deque
 N, M, Start = map(int, input().split())
 A = [[] for _ in range(N + 1)]
 
-for _ in range(M): # 그래프 저장
+for _ in range(M):
     s, e = map(int, input().split())
     A[s].append(e)
     A[e].append(s)
 
-for i in range(N + 1): # 그래프 오름차순 정렬
-    A[i].sort() # 예: [[], [2, 3], [5, 1], [4, 1], [5, 3], [4, 2]]는 [[], [2, 3], [1, 5], [1, 4], [3, 5], [2, 4]]로 정렬됨
+for i in range(N + 1):
+    A[i].sort()
 
 def BFS(v):
     queue = deque()
@@ -23,7 +21,7 @@ def BFS(v):
     while queue:
         now_Node = queue.popleft()
         print(now_Node, end=' ')
-        for i in A[now_Node]: # 예: [1, 4]이면, queue에는 1과 4 개개로 들어감
+        for i in A[now_Node]:
             if not visited[i]:
                 visited[i] = True
                 queue.append(i)
