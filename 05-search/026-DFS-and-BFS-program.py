5fe23178e91abcb598c8fa746cd2223b9ffab9a1
sarifor <iread1q84@gmail.com>
2023-08-05 11:53:57 +0900

    Add Explanation for 026

--- 05-search/026-DFS-and-BFS-program.py
+++ 05-search/026-DFS-and-BFS-program.py
@@ -1,16 +1,17 @@
 # Backjoon 1260
+# 원래 문제의 DFS 부분 삭제
 
 from collections import deque
 N, M, Start = map(int, input().split())
 A = [[] for _ in range(N + 1)]
 
-for _ in range(M):
+for _ in range(M): # 그래프 저장
     s, e = map(int, input().split())
     A[s].append(e)
     A[e].append(s)
 
-for i in range(N + 1):
-    A[i].sort()
+for i in range(N + 1): # 그래프 오름차순 정렬
+    A[i].sort() # 예: [[], [2, 3], [5, 1], [4, 1], [5, 3], [4, 2]]는 [[], [2, 3], [1, 5], [1, 4], [3, 5], [2, 4]]로 정렬됨
 
 def BFS(v):
     queue = deque()
@@ -19,7 +20,7 @@ def BFS(v):
     while queue:
         now_Node = queue.popleft()
         print(now_Node, end=' ')
-        for i in A[now_Node]:
+        for i in A[now_Node]: # 예: [1, 4]이면, queue에는 1과 4 개개로 들어감
             if not visited[i]:
                 visited[i] = True
                 queue.append(i)
