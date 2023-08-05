01bee9ed797bbd05774b65c8c9f19f8e9fa82ccc
sarifor <iread1q84@gmail.com>
2023-08-05 09:41:24 +0900

    Add English explanation for map

--- python-grammars/map.py
+++ python-grammars/map.py
@@ -1,39 +1,39 @@
-# for 문의 경우
-a = [1.2, 2.5, 3.7, 4.6] # 리스트
+# for statement
+a = [1.2, 2.5, 3.7, 4.6] # list
 for i in range(len(a)):
     a[i] = int(a[i])
 print(a) # [1, 2, 3, 4]
 
 
-# map 함수의 경우 (더 간단)
-b = [1.2, 2.5, 3.7, 4.6] # 리스트
+# map function (simpler)
+b = [1.2, 2.5, 3.7, 4.6] # list
 b = list(map(int, a))
 print(b) # [1, 2, 3, 4]
 
-c = list(map(str, range(10))) # 리스트 외의 반복 객체
+c = list(map(str, range(10))) # iterable besides list
 print(c) # ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 
-# input().split()과 map
-d = input().split() # 10 20 입력
-print(d) # ['10', '20'] (input().split()은 리스트를 반환)
+# input().split() and map
+d = input().split() # Input 10 20
+print(d) # ['10', '20'] (input().split() returns list)
 
-e = map(int, input().split()) # 10 20 입력
+e = map(int, input().split()) # Input 10 20
 print(e) # <map object at 0x0000017AACB6BFA0>
 print(list(e)) # [10, 20]
 
 
-# 리스트를 변수 두 개에 저장
+# Save list into two variables
 f, g = [10, 20]
 print(f) # 10
 print(g) # 20
 
-h, i = map(int, input().split()) # 10 20 입력
+h, i = map(int, input().split()) # Input 10 20
 print(h) # 10
 print(i) # 20
 
-j = input().split() # 10 20 입력
+j = input().split() # Input 10 20
 k = map(int, j)
-l, m = k # map 객체는 이터레이터라서 변수 여러 개에 저장하는 언패킹이 가능하므로, list(k)를 생략
+l, m = k # map object is iterator so it can be unpackaged into more than one variable, which means list(k) can be omitted.
 print(l) # 10
 print(m) # 20
