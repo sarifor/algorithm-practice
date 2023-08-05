363bb493636be7ff150fc8e98cb51b3a34ffbdc8
sarifor <iread1q84@gmail.com>
2023-08-05 12:43:39 +0900

    Add Explanation for 029

--- 05-search/029-find-target-numbers.py
+++ 05-search/029-find-target-numbers.py
@@ -1,4 +1,8 @@
 # Backjoon 1920
+# 데이터가 정렬되어 있는 상태에서 원하는 값을 찾는다.
+# 대상 데이터의 중앙값과 찾고자 하는 값을 비교해,
+# 데이터의 크기를 절반씩 줄이면서 대상을 찾고,
+# '중앙값 == 타깃 데이터'일 때 탐색을 종료한다.
 
 N = int(input())
 A = list(map(int, input().split()))
@@ -14,7 +18,7 @@ for i in range(M):
     end = len(A) - 1
     while start <= end:
         midi = int((start + end) / 2)
-        midv = A[midi] # 중앙값
+        midv = A[midi]
         if midv > target:
             end = midi - 1
         elif midv < target:
