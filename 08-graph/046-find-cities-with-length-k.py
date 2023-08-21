8842bb262ea8d7a1398b2cb5a3cd247e6ab1ec32
sarifor <iread1q84@gmail.com>
2023-08-21 09:51:05 +0900

    Add English explanation for 046

--- 08-graph/046-find-cities-with-length-k.py
+++ 08-graph/046-find-cities-with-length-k.py
@@ -1,36 +1,40 @@
 # Backjoon 18352
+# Find vertices that have distance of K using BFS.
+# For each neighbor of the dequeued vertex that has not been visited,
+# set its distance to the distance of the dequeued vertext plus 1
+# and add it to the queue.
 
 import sys
 from collections import deque
 input = sys.stdin.readline
 
-N, M, K, X = map(int, input().split()) # 노드 수, 에지 수, 목표 거리, 시작점
-A = [[] for _ in range(N + 1)] # 인접 리스트
+N, M, K, X = map(int, input().split())
+A = [[] for _ in range(N + 1)]
 answer = []
-visited = [-1] * (N + 1) # 방문 리스트
+visited = [-1] * (N + 1)
 
 def BFS(v):
     queue = deque()
     queue.append(v)
-    visited[v] += 1 # 최초 노드는 거리 0
+    visited[v] += 1
     while queue:
         now_Node = queue.popleft()
         for i in A[now_Node]:
-            if visited[i] == -1: # 현재 노드의 연결 노드 중 방문하지 않은 노드가 있다면
-                visited[i] = visited[now_Node] + 1 # '이전 도시의 방문 리스트값 + 1' 저장
+            if visited[i] == -1:
+                visited[i] = visited[now_Node] + 1
                 queue.append(i)
         
-for _ in range(M): # 그래프 데이터 저장
+for _ in range(M):
     S, E = map(int, input().split())
     A[S].append(E)
 
-BFS(X) # 너비 우선 탐색
+BFS(X)
 
-for i in range(N + 1): # 방문 거리가 K인 노드를 정답 리스트에 더함
+for i in range(N + 1):
     if visited[i] == K:
         answer.append(i)
 
-if not answer: # 정답 출력
+if not answer:
     print(-1)
 else:
     answer.sort()
