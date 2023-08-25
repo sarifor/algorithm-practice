452f6ebcead8d3b444d30a4c0d01e38dd27f9151
sarifor <iread1q84@gmail.com>
2023-08-25 10:49:00 +0900

    Add explanation for 053

--- 08-graph/053-line-up-students-by-height.py
+++ 08-graph/053-line-up-students-by-height.py
@@ -1,24 +1,32 @@
+# Backjoon 2252
+# Pick all the verticles with in-degree as 0,
+# add them into a queue,
+# remove a vertex from the queue and print it,
+# decrease in-degree by 1 for all its neighbouring nodes,
+# and if the in-degree of neighbouring nodes is reduced to zero, then add it to the queue.
+# Repeat steps above until the queue is empty
+
 from collections import deque
 
-N, M = map(int, input().split())  # 노드 수, 비교 횟수
-A = [[] for _ in range(N + 1)]  # 인접 리스트 배열
-indegree = [0] * (N + 1)  # 진입차수 배열
+N, M = map(int, input().split())
+A = [[] for _ in range(N + 1)] 
+indegree = [0] * (N + 1)
 
 for i in range(M):
-  S, E = map(int, input().split())  # 인접 리스트 데이터
-  A[S].append(E)  # 인접 리스트 데이터 저장
-  indegree[E] += 1  # 진입차수 데이터 저장
+  S, E = map(int, input().split())
+  A[S].append(E)
+  indegree[E] += 1
 
 queue = deque()
 
-for i in range(1, N + 1):  # 진입 차수 0인 노드를 큐에 삽입
+for i in range(1, N + 1):
   if indegree[i] == 0:
     queue.append(i)
 
 while queue:
   now = queue.popleft()
-  print(now, end=' ')  # 현재 노드값 출력
+  print(now, end=' ')
   for next in A[now]:
-    indegree[next] -= 1  # 현재 노드의 타깃 노드의 진입 차수값 1 감소
-    if indegree[next] == 0:  # 타깃 노드의 진입 차수가 0이면
-      queue.append(next)  # 큐에 타깃 노드 추가
+    indegree[next] -= 1
+    if indegree[next] == 0:
+      queue.append(next)
