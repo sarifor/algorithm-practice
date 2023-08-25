a2e2f1f5b1c7fff7b393ed41adfbfe70ba336b70
sarifor <iread1q84@gmail.com>
2023-08-25 12:43:16 +0900

    Improve explanation for 054

--- 08-graph/054-calculate-minimum-time-required-to-build-each-building.py
+++ 08-graph/054-calculate-minimum-time-required-to-build-each-building.py
@@ -8,7 +8,7 @@ indegree = [0] * (N + 1) # 진입 차수 리스트
 selfBuild = [0] * (N + 1) # 자기 자신을 짓는 데 걸리는 시간 저장 리스트
 
 for i in range(1, N + 1): # 건물의 수만큼 반복
-    # 인접 리스트 데이터 저장
+    # 데이터 입력받음
     # ex. [10, 1, -1]: 건물 짓는 데 걸리는 시간, 먼저 지어야 하는 건물들의 번호, 각 줄이 끝남을 표시
     inputList = list(map(int, input().split()))
 
@@ -21,18 +21,18 @@ for i in range(1, N + 1): # 건물의 수만큼 반복
         index += 1
         if preTemp == -1:
           break
-        A[preTemp].append(i)
+        A[preTemp].append(i) # 인접 리스트 데이터 저장
         indegree[i] += 1 # 진입 차수 리스트 데이터 저장
 
 queue = deque()
 
-for i in range(1, N + 1): # 건물 수만큼 반복
-   if indegree[i] == 0:
+for i in range(1, N + 1):
+   if indegree[i] == 0: # 진입 차수 리스트의 값이 0인 건물(노드)을 큐에 삽입
       queue.append(i)
 
-result = [0] * (N + 1) # 위상 정렬 결과를 저장할 배열
+result = [0] * (N + 1) # 정답 리스트
 
-while queue: # 위상 정렬 수행
+while queue: # 각 건물을 짓는 데 걸리는 최대 시간 업데이트
    now = queue.popleft()
    for next in A[now]:
       indegree[next] -= 1
@@ -40,5 +40,6 @@ while queue: # 위상 정렬 수행
       if indegree[next] == 0:
          queue.append(next)
 
-for i in range(1, N + 1): # 위상 정렬 결과 출력
+# 정답 리스트에 자기 건물을 짓는 데 걸리는 시간을 더한 후, 정답 리스트를 차례대로 출력
+for i in range(1, N + 1):
    print(result[i] + selfBuild[i])
