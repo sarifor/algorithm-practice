fae22b0ce200d8ac6c96a33c8e5ebf8ecbe1f26a
sarifor <iread1q84@gmail.com>
2023-08-28 15:29:58 +0900

    Add explanation for 058

--- 08-graph/058-find-k-shortest-paths.py
+++ 08-graph/058-find-k-shortest-paths.py
@@ -11,11 +11,11 @@ for _ in range(M):
     a, b, c = map(int, input().split()) # a에서 b로 가고, 가중치 c 부여
     W[a].append((b, c)) # 인접 리스트에 에지 정보 저장
 
-pq = [(0, 1)] # 우선순위 큐(heapq)에 시작 노드(가중치, 노드) 저장
-distance[1][0] = 0 # 시작 도시 최단 거리 저장
+pq = [(0, 1)] # 우선순위 큐(heapq)에 '시작 도시의 최단 거리'와 '시작 도시' 저장
+distance[1][0] = 0 # 시작 도시의 최단 거리 저장
 
 while pq:
-    cost, node = heapq.heappop(pq)
+    cost, node = heapq.heappop(pq) # 거리 값이 제일 적은 노드가 선택됨
     for nNode, nCost in W[node]: # 현재 노드의 연결 노드와 가중치를 꺼내서
         sCost = cost + nCost # 새로운 총 거리를 구하고
         if distance[nNode][K-1] > sCost: # 연결 노드의 K번째 경로와 신규 경로 비교
