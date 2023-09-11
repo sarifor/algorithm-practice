90b06e761fd83ed8975ab0ba3362319a66caed75
sarifor <iread1q84@gmail.com>
2023-09-11 14:19:07 +0900

    Remove Korean notes for 061

--- 08-graph/061-find-the-shortest-path-between-all-nodes.py
+++ 08-graph/061-find-the-shortest-path-between-all-nodes.py
@@ -6,28 +6,24 @@
 import sys
 
 input = sys.stdin.readline
-N = int(input())  # 도시 개수
-M = int(input())  # 노선 개수
-distance = [[sys.maxsize for j in range(N + 1)] for i in range(N + 1)]  # 인접 행렬
+N = int(input())
+M = int(input())
+distance = [[sys.maxsize for j in range(N + 1)] for i in range(N + 1)]
 
-# 시작 도시와 종료 도시가 같은 자리에 0 저장
 for i in range(1, N + 1):
   distance[i][i] = 0
 
-# 노선 데이터를 인접 행렬에 저장
 for i in range(M):
   s, e, v = map(int, input().split())
   if distance[s][e] > v:
     distance[s][e] = v
 
-# 전체 경로의 최단 경로가 부분 경로의 최단 경로의 조합보다 클 경우, 후자로 업데이트
 for k in range(1, N + 1):
   for i in range(1, N + 1):
     for j in range(1, N + 1):
       if distance[i][j] > distance[i][k] + distance[k][j]:
         distance[i][j] = distance[i][k] + distance[k][j]
 
-# 정답 출력
 for i in range(1, N + 1):
   for j in range(1, N + 1):
     if distance[i][j] == sys.maxsize:
