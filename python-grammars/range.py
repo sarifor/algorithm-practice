4a6d6787a1ebc21fc1e03d95fdb15688aac9d7fd
sarifor <iread1q84@gmail.com>
2023-10-19 12:21:54 +0900

    Pratice range

---
+++ python-grammars/range.py
@@ -0,0 +1,14 @@
+# range(횟수)
+for i in range(10):
+  print(i) # 0 1 2 3 4 5 6 7 8 9
+
+# range(시작, 끝)
+for i in range(1, 10):
+  print(i) # 1 2 3 4 5 6 7 8 9
+
+# range(시작, 끝, 증가폭)
+for i in range(1, 10, 2):
+  print(i) # 1 3 5 7 9
+
+for i in range(10, -1, -1):
+  print(i) # 9 8 7 6 5 4 3 2 1 0
