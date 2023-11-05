9ebd009eb660bb1f2dcdac95d270614a506284ec
sarifor <iread1q84@gmail.com>
2023-11-05 17:45:17 +0900

    Try understanding merge sort

--- 04-sort/020-sort-numbers-2.py
+++ 04-sort/020-sort-numbers-2.py
@@ -12,11 +12,20 @@ def merge_sort(s, e):
     if e - s < 1: return
     
     m = int(s + (e - s) / 2)
+    print("Start: " + str(s) + '\n')
+    print("End: " + str(e) + '\n')
+    print("Mid: " + str(m) + '\n')
+
     merge_sort(s, m)
     merge_sort(m + 1, e)
 
+    print("Start after func.: " + str(s) + '\n')
+    print("End after func.: " + str(e) + '\n')
+
     for i in range(s, e + 1):
         tmp[i] = A[i]
+
+    print("tmp after func.: " + str(tmp) + '\n')
 
     k = s
     index1 = s
@@ -42,14 +51,18 @@ def merge_sort(s, e):
         k += 1
         index2 += 1
 
+    print("tmp: " + str(tmp) + '\n')
+    print("A: " + str(A) + '\n')
+    print("----" + '\n')
+
 N = int(input())
 A = [0] * int(N + 1)
 tmp = [0] * int(N + 1)
 
-for i in range(1, N + 1):
+for i in range(1, N + 1): # 인덱스 1부터 시작
     A[i] = int(input())
 
 merge_sort(1, N)
 
-for i in range(1, N + 1):
+for i in range(1, N + 1): # 인덱스 1부터 시작
     print(str(A[i]) + '\n')
